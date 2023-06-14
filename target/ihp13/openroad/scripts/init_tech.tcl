94c2dcc7365654a6d9ca547ad464edbd3ca12392
Jannis Schönleber <joennlae@gmail.com>
2023-06-14 10:29:16 +0200

    openroad: Clean up technology cell read-in
    * cleanup: add `lef` patch + cleanup
    
    * cleanup: patched srams now in common/patched/technology/srams
    
    ---------
    
    Co-authored-by: Tobias Senti <tsenti@student.ethz.ch>

--- target/ihp13/openroad/scripts/init_tech.tcl
+++ target/ihp13/openroad/scripts/init_tech.tcl
@@ -11,23 +11,23 @@ read_liberty -corner tt ../technology/lib/plankton_typ_1p2V_3p3V_25C.lib
 read_liberty -corner ff ../technology/lib/plankton_fast_1p32V_3p6V_m40C.lib
 
 # Read Patched SRAMs
-read_liberty -corner tt ./src/RM_IHPSG13_1P_64x64_c2_bm_bist_tc_1d20V_25C.lib
-#read_liberty -corner ss ./src/RM_IHPSG13_1P_64x64_c2_bm_bist_wc_1d08V_125C.lib
-read_liberty -corner ff ./src/RM_IHPSG13_1P_64x64_c2_bm_bist_bc_1d32V_m55C.lib
+read_liberty -corner tt ../../common/patched/technology/srams/RM_IHPSG13_1P_64x64_c2_bm_bist_tc_1d20V_25C.lib
+#read_liberty -corner ss ../../common/patched/technology/srams/RM_IHPSG13_1P_64x64_c2_bm_bist_wc_1d08V_125C.lib
+read_liberty -corner ff ../../common/patched/technology/srams/RM_IHPSG13_1P_64x64_c2_bm_bist_bc_1d32V_m55C.lib
 
-read_liberty -corner tt ./src/RM_IHPSG13_1P_256x64_c2_bm_bist_tc_1d20V_25C.lib
-#read_liberty -corner ss ./src/RM_IHPSG13_1P_256x64_c2_bm_bist_wc_1d08V_125C.lib
-read_liberty -corner ff ./src/RM_IHPSG13_1P_256x64_c2_bm_bist_bc_1d32V_m55C.lib
+read_liberty -corner tt ../../common/patched/technology/srams/RM_IHPSG13_1P_256x64_c2_bm_bist_tc_1d20V_25C.lib
+#read_liberty -corner ss ../../common/patched/technology/srams/RM_IHPSG13_1P_256x64_c2_bm_bist_wc_1d08V_125C.lib
+read_liberty -corner ff ../../common/patched/technology/srams/RM_IHPSG13_1P_256x64_c2_bm_bist_bc_1d32V_m55C.lib
 
-read_liberty -corner tt ./src/RM_IHPSG13_1P_1024x64_c2_bm_bist_tc_1d20V_25C.lib
-#read_liberty -corner ss ./src/RM_IHPSG13_1P_1024x64_c2_bm_bist_wc_1d08V_125C.lib
-read_liberty -corner ff ./src/RM_IHPSG13_1P_1024x64_c2_bm_bist_bc_1d32V_m55C.lib
+read_liberty -corner tt ../../common/patched/technology/srams/RM_IHPSG13_1P_1024x64_c2_bm_bist_tc_1d20V_25C.lib
+#read_liberty -corner ss ../../common/patched/technology/srams/RM_IHPSG13_1P_1024x64_c2_bm_bist_wc_1d08V_125C.lib
+read_liberty -corner ff ../../common/patched/technology/srams/RM_IHPSG13_1P_1024x64_c2_bm_bist_bc_1d32V_m55C.lib
 
 # tech lef
 read_lef ../technology/lef/sg13g2_tech.lef
 
 # cell lef
-read_lef ./src/sg13g2_stedwardell.lef
+read_lef ../../../common/patched/technology/sg13g2_stedwardell.lef
 read_lef ../technology/lef/plankton_v5p8.lef
 read_lef ../technology/lef/RM_IHPSG13_1P_64x64_c2_bm_bist.lef
 read_lef ../technology/lef/RM_IHPSG13_1P_256x64_c2_bm_bist.lef
