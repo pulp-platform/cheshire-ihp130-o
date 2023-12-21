160f1c237decc3f309979436ad45250578ea84da
Philippe Sauter <phsauter@student.ethz.ch>
2023-12-21 16:36:21 +0100

    hw: update pdk, add new srams

--- target/ihp13/openroad/scripts/init_tech.tcl
+++ target/ihp13/openroad/scripts/init_tech.tcl
@@ -21,14 +21,23 @@ read_liberty -corner tt ../pdk/future/sg13g2_iocell/sg13g2_iocell_typ_1p2V_3p3V_
 read_liberty -corner ff ../pdk/future/sg13g2_iocell/sg13g2_iocell_fast_1p32V_3p3V_m40C.lib
 
 # Read Patched SRAMs TODO: add proper corners when released!
-read_liberty -corner tt ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_64x64_c2_bm_bist_dummy.lib
-read_liberty -corner ff ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_64x64_c2_bm_bist_dummy.lib
+read_liberty -corner tt ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_64x64_c2_bm_bist_typ_1p20V_25C.lib
+read_liberty -corner ff ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_64x64_c2_bm_bist_fast_1p32V_m55C.lib
 
-read_liberty -corner tt ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_256x64_c2_bm_bist_dummy.lib
-read_liberty -corner ff ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_256x64_c2_bm_bist_dummy.lib
+read_liberty -corner tt ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_256x48_c2_bm_bist_typ_1p20V_25C.lib
+read_liberty -corner ff ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_256x48_c2_bm_bist_fast_1p32V_m55C.lib
 
-read_liberty -corner tt ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_1024x64_c2_bm_bist_dummy.lib
-read_liberty -corner ff ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_1024x64_c2_bm_bist_dummy.lib
+read_liberty -corner tt ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_256x64_c2_bm_bist_typ_1p20V_25C.lib
+read_liberty -corner ff ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_256x64_c2_bm_bist_fast_1p32V_m55C.lib
+
+read_liberty -corner tt ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_512x64_c2_bm_bist_typ_1p20V_25C.lib
+read_liberty -corner ff ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_512x64_c2_bm_bist_fast_1p32V_m55C.lib
+
+read_liberty -corner tt ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_1024x64_c2_bm_bist_typ_1p20V_25C.lib
+read_liberty -corner ff ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_1024x64_c2_bm_bist_fast_1p32V_m55C.lib
+
+read_liberty -corner tt ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_2048x64_c2_bm_bist_typ_1p20V_25C.lib
+read_liberty -corner ff ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lib/RM_IHPSG13_1P_2048x64_c2_bm_bist_fast_1p32V_m55C.lib
 
 # Delay Line
 read_liberty -corner tt ../src/mc_delay/delay_line_D4_O1_6P000.mid_guess.lib
@@ -40,9 +49,12 @@ read_lef ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_stdcell/lef/sg13g2_tech.le
 # cell lef
 read_lef ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_stdcell/lef/sg13g2_stdcell.lef
 read_lef ../pdk/future/sg13g2_iocell/sg13g2_iocell.lef
-read_lef ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_64x64_c2_bm_bist.lef
-read_lef ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_256x64_c2_bm_bist.lef
-read_lef ../pdk/future/sg13g2_sram/RM_IHPSG13_1P_1024x64_c2_bm_bist.lef
+read_lef ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lef/RM_IHPSG13_1P_64x64_c2_bm_bist.lef
+read_lef ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lef/RM_IHPSG13_1P_256x48_c2_bm_bist.lef
+read_lef ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lef/RM_IHPSG13_1P_256x64_c2_bm_bist.lef
+read_lef ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lef/RM_IHPSG13_1P_512x64_c2_bm_bist.lef
+read_lef ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lef/RM_IHPSG13_1P_1024x64_c2_bm_bist.lef
+read_lef ../pdk/ihp-sg13g2/ihp-sg13g2/libs.ref/sg13g2_sram/lef/RM_IHPSG13_1P_2048x64_c2_bm_bist.lef
 read_lef ../src/mc_delay/delay_line_D4_O1_6P000.abst.lef
 
 set ctsBuf [ list sg13g2_buf_16 sg13g2_buf_8 sg13g2_buf_4 ]
