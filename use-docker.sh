8d42582fdfb957ac9e850a5aea84d067318e8484
Philippe Sauter <phsauter@student.ethz.ch>
2024-01-15 11:08:45 +0100

    docker: fix displaying of gui

--- use-docker.sh
+++ use-docker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd docker && make pull
-env UID=${UID} GID=${GID} docker-compose run pulp-iguana
+
+env UID=${UID} GID=${GID} docker-compose run \
+  -e PS1="\[\033[01;32m\]docker-pulp: \[\033[00m\]\[\033[01;34m\]\w\[\033[00m\] $" \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  pulp-iguana
