feb59328efca5173a706dd9e7ee3213bc960bd17
Philippe Sauter <phsauter@student.ethz.ch>
2024-04-18 09:36:08 +0200

    hw: add split bootrom script
    Splitting the bootrom into parts creates separate amoebae in placement.
    This reduces local routing congestion as it is less interconnected.
    The trade-off is that the area increases with additional splits.
    Recommended: 1,2 or 4 parts

--- target/sim/vsim/compile.bootrom.rtl.tcl
+++
@@ -1,5 +0,0 @@
-set ROOT "/home/msc23h5/repos/basilisk"
-vlog -sv "$ROOT/.bender/git/checkouts/cheshire-4d602a690802c1f8/hw/bootrom/cheshire_bootrom.sv"
-vlog -sv +define+NO_BOOTROM_WRAP "$ROOT/hw/cheshire_bootrom_split.sv"
-vlog -sv "$ROOT/target/sim/src/tb_cheshire_bootrom.sv"
-exit
